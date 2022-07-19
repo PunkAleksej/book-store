2816583f75a31558790fc78840db54468dafb885
Alex Dudnikov <alekdudnikov@mail.ru>
2022-07-19 17:57:01 +0300

    fff

---
+++ src/components/mainPage/mainFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div>
+      <button>SignIn</button>
+      <button>signUp</button>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/mainPage/mainHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div>
+      <button>SignIn</button>
+      <button>signUp</button>
+    </div>
+  );
+}
+
+export default Header;
